359e895b0a30ce08b8d0c5a9c72b7e2acc5865ce
pnadolny13 <patnadolny@gmail.com>
2024-01-31 08:59:25 -0500

    support key_file_str and write to a temp file

--- README.md
+++ README.md
@@ -14,3 +14,19 @@ Sample config:
   "start_date": "2019-02-01T00:00:00Z"
 }
 ```
+
+Optionally `key_file_str` can be used instead of `key_file`.
+If `key_file_str` is provided it will be written to a temporary file and used as the key file, then later deleted on exit.
+The input of that key must have new line characters `\n` for the line breaks in the key file contents.
+
+An example could look like:
+
+```$json
+{
+  "key_id": "AAAAAAAAA",
+  "key_file_str": ".-----BEGIN PRIVATE KEY-----\nfoo\nbar\nbaz\nother\n-----END PRIVATE KEY-----",
+  "issuer_id": "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa",
+  "vendor": "3333333",
+  "start_date": "2019-02-01T00:00:00Z"
+}
+```
--- tap_appstore/__init__.py
+++ tap_appstore/__init__.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
+import atexit
 import json
+import os
+import tempfile
 
 import singer
 from appstoreconnect import Api
-from singer import utils, Catalog
+from singer import Catalog, utils
 
 from tap_appstore.discover import discover, do_discover
 from tap_appstore.sync import sync
 
 REQUIRED_CONFIG_KEYS = [
     'key_id',
-    'key_file',
     'issuer_id',
     'vendor',
     'start_date'
@@ -18,6 +20,21 @@ REQUIRED_CONFIG_KEYS = [
 
 LOGGER = singer.get_logger()
 
+def _delete_temp_file(temp_filename) -> None:
+    try:
+        LOGGER.info(f"Cleaning up: {temp_filename}")
+    finally:
+        os.remove(temp_filename)
+        LOGGER.info(f"Temporary key_file deleted: {temp_filename}")
+
+def _create_temp_key_file(key_file_str) -> str:
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    temp_filename = temp_file.name
+    with open(temp_filename, "w") as file:
+        file.write(key_file_str)
+    LOGGER.info(f"Temporary key_file created: {temp_filename}")
+    atexit.register(_delete_temp_file, temp_filename)
+    return temp_filename
 
 @utils.handle_top_exception(LOGGER)
 def main():
@@ -25,6 +42,12 @@ def main():
     args = utils.parse_args(REQUIRED_CONFIG_KEYS)
 
     config = args.config
+    if "key_file" not in config and "key_file_str" not in config:
+        raise Exception("Config is missing required keys: key_file or key_file_str.")
+
+    if config.get('key_file_str'):
+        config['key_file'] = _create_temp_key_file(config.pop("key_file_str"))
+
     client = Api(config['key_id'], config['key_file'], config['issuer_id'], submit_stats=False)
 
     state = {}
